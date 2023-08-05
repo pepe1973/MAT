9b1f804ed64fc0528115ff53dbf8f9cf6f6ceac5
pepe1973 <bodrogipr@gmail.com>
2023-08-05 19:28:40 +0200

    JavaScript 2023.08.05-2

---
+++ sablonok_mintak_anyagok/javascript.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('h1')[0].innerText = 'Változtatás!';
+document.getElementsByTagName('h2')[0].innerText = 'Második változtatás!';
+document.getElementsByTagName('h3')[0].innerText = 'Harmadik változtatás!';
