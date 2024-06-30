bf487871b05c629188ca72f69d3826c3f54c21ef
pepe1973 <bodrogipr@gmail.com>
2024-06-30 20:29:23 +0200

    2024.06.30. Első mentés

--- src/js/mozgat.js
+++ src/js/mozgat.js
@@ -1,15 +1,33 @@
-let rejtettek = document.getElementsByClassName("rejtett");
-let felfedGombok = document.getElementsByClassName("felfed-gomb");
-let rejtoGombok = document.getElementsByClassName("elrejt-gomb");
+let rejtettek = document.getElementsByClassName('rejtett');
+let felfedGombok = document.getElementsByClassName('felfed-gomb');
+// let mindenFelfedGomb = document.getElementsByClassName('mindenfelfed-gomb');
+let rejtoGombok = document.getElementsByClassName('elrejt-gomb');
+// let mindenRejtoGomb = document.getElementsByClassName('mindenelrejt-gomb');
 
 function felfed(index) {
-  rejtettek[index].style.display = "block";
-  felfedGombok[index].style.display = "none";
-  rejtoGombok[index].style.display = "inline";
+    rejtettek[index].style.display = 'block';
+    felfedGombok[index].style.display = 'none';
+    rejtoGombok[index].style.display = 'inline';
 }
 
 function elrejt(index) {
-  rejtettek[index].style.display = "none";
-  felfedGombok[index].style.display = "inline";
-  rejtoGombok[index].style.display = "none";
+    rejtettek[index].style.display = 'none';
+    felfedGombok[index].style.display = 'inline';
+    rejtoGombok[index].style.display = 'none';
+}
+
+function mindenFelfed() {
+    for (let index = 0; index < rejtettek.length; index++) {
+        rejtettek[index].style.display = 'block';
+        felfedGombok[index].style.display = 'none';
+        rejtoGombok[index].style.display = 'inline';
+    }
+}
+
+function mindenElrejt() {
+    for (let index = 0; index < rejtettek.length; index++) {
+        rejtettek[index].style.display = 'none';
+        felfedGombok[index].style.display = 'inline';
+        rejtoGombok[index].style.display = 'none';
+    }
 }
